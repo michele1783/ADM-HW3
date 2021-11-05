0d55ee9ef6d0fdc137da91f1abcbbc0ba4e47b98
michele1783 <72614929+michele1783@users.noreply.github.com>
2021-11-05 09:40:02 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -11,6 +11,7 @@ import shutil
 from bs4 import BeautifulSoup
 from pathlib import Path
 import requests
+from tqdm import tqdm
     
 def crawl(url_file):
     Path("directory").mkdir(exist_ok=True)
@@ -36,3 +37,18 @@ def crawl(url_file):
             number = 4
         shutil.move("page_{}.html".format(c), "/Users/michele/ADM2021/cartella{}".format(number))
         f.close()
+
+def get_urls(init_url, number_pages):
+    anime = []
+    for page in tqdm(range(0,number_pages)):
+        url = init_url +str(page *50)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        for tag in soup.find_all("tr"):#cerco le righe della tabella
+            links = tag.find_all("a")
+            for link in links:
+                if type(link.get("id")) == str and len(link.contents[0]) >1:
+                    anime.append((link.get("href")))
+    with open('urls.txt', 'w') as f:
+        for item in tqdm(anime):
+            f.write("%s\n" % item)  
